6b67568ed81389ef4a3f767da62d240a3c27698b
FairyBanished <liyanjin3967@yeah.net>
2019-11-19 11:29:38 +0800

    测

--- src/main/java/com/lee/fairy/banish/controller/JenkinsController.java
+++ src/main/java/com/lee/fairy/banish/controller/JenkinsController.java
@@ -11,7 +11,7 @@ public class JenkinsController {
 	@RequestMapping("/data")
 	@ResponseBody
 	public String data() {
-		return "你好,这是一条 Jenkins数据,嚯嚯嚯嚯嚯嚯嚯嚯嚯！ ";
+		return "你好,这是一条 Jenkins数据,嚯嚯嚯，嚯嚯嚯，嚯嚯嚯！ ";
 	}
 
 }
